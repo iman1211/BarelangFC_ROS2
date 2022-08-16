48d7806cfa64dda110c9b84d6bd8001987874b56
iman1211 <imrofsy31@gmail.com>
2022-08-16 11:40:05 +0700

    add sensor

--- src/bfc_motion_bridge/src/bfc_motion_bridge.cpp
+++ src/bfc_motion_bridge/src/bfc_motion_bridge.cpp
@@ -5,6 +5,7 @@
 #include "std_msgs/msg/float32_multi_array.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "bfc_msgs/msg/head_movement.hpp"
+#include "bfc_msgs/msg/button.hpp"
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -61,36 +62,52 @@ void runLuaProgram()
 class motion_bridge : public rclcpp::Node
 {
 public:
-    motion_bridge() : Node("motion_bridge")
-    {
-        RCLCPP_INFO(this->get_logger(), "motion_bridge started");
-        walk_command_ = this->create_subscription<geometry_msgs::msg::Twist>(
-            "robot_walk", 10,
-            std::bind(&motion_bridge::walkCommand, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "robot_walk has been started.");
-
-        head_command_ = this->create_subscription<bfc_msgs::msg::HeadMovement>(
-            "robot_head", 10,
-            std::bind(&motion_bridge::headCommand, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "robot_head has been started.");
-
-        motion_command_ = this->create_subscription<std_msgs::msg::String>(
-            "robot_motion", 10,
-            std::bind(&motion_bridge::motionCommand, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "robot_motion has been started.");
-    }
+    motion_bridge();
     void walkCommand(const geometry_msgs::msg::Twist::SharedPtr msg);
     void headCommand(const bfc_msgs::msg::HeadMovement::SharedPtr msg);
     void motionCommand(const std_msgs::msg::String::SharedPtr msg);
     void motion(char line[2]);
     double Walk(double x, double y, double a);
     void headMove(double pan, double tilt);
+    void publishButton();
+    void publishImu();
+    void getSensor();
 
 private:
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr walk_command_;
     rclcpp::Subscription<bfc_msgs::msg::HeadMovement>::SharedPtr head_command_;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr motion_command_;
+    rclcpp::Publisher<bfc_msgs::msg::Button>::SharedPtr button_state_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_state_;
+    rclcpp::TimerBase::SharedPtr timer_button_;
+    rclcpp::TimerBase::SharedPtr timer_imu_;
+
+    double accelero_x, accelero_y, accelero_z, gyroscope_x, gyroscope_y, gyroscope_z = 0;
+    int roll, pitch, yaw, strategy, kill = 0;
 };
+
+motion_bridge::motion_bridge() : Node("motion_bridge")
+{
+    RCLCPP_INFO(this->get_logger(), "motion_bridge started");
+    walk_command_ = this->create_subscription<geometry_msgs::msg::Twist>(
+        "robot_walk", 10,
+        std::bind(&motion_bridge::walkCommand, this, std::placeholders::_1));
+    RCLCPP_INFO(this->get_logger(), "robot_walk has been started.");
+
+    head_command_ = this->create_subscription<bfc_msgs::msg::HeadMovement>(
+        "robot_head", 10,
+        std::bind(&motion_bridge::headCommand, this, std::placeholders::_1));
+    RCLCPP_INFO(this->get_logger(), "robot_head has been started.");
+
+    motion_command_ = this->create_subscription<std_msgs::msg::String>(
+        "robot_motion", 10,
+        std::bind(&motion_bridge::motionCommand, this, std::placeholders::_1));
+    RCLCPP_INFO(this->get_logger(), "robot_motion has been started.");
+
+    button_state_ = this->create_publisher<bfc_msgs::msg::Button>("robot_button", 10);
+    timer_button_ = this->create_wall_timer(std::chrono::milliseconds(50),
+                                            std::bind(&motion_bridge::publishButton, this));
+}
 
 void motion_bridge::walkCommand(const geometry_msgs::msg::Twist::SharedPtr msg)
 {
@@ -105,6 +122,23 @@ void motion_bridge::headCommand(const bfc_msgs::msg::HeadMovement::SharedPtr msg
 void motion_bridge::motionCommand(const std_msgs::msg::String::SharedPtr msg)
 {
     motion(&msg->data[0]);
+}
+
+void motion_bridge::publishButton()
+{
+    auto msg = bfc_msgs::msg::Button();
+    msg.strategy = strategy;
+    msg.kill = kill;
+    button_state_->publish(msg);
+}
+
+void motion_bridge::publishImu()
+{
+    auto msg = sensor_msgs::msg::Imu();
+    msg.angular_velocity.x = roll;
+    msg.angular_velocity.y = pitch;
+    msg.angular_velocity.z = yaw;
+    imu_state_->publish(msg);
 }
 
 void motion_bridge::motion(char line[2])
@@ -131,6 +165,69 @@ void motion_bridge::headMove(double pan, double tilt)
     sprintf(dataHead, "%.2f,%.2f", pan, tilt);
     sendto(sockhead, dataHead, strlen(dataHead), 0, (struct sockaddr *)&addrhead, sizeof(addrhead));
     printf("  data head = %s\n", dataHead);
+}
+
+void motion_bridge::getSensor()
+{
+    char line[100];
+    char *spr;
+    int countParse = 0;
+
+    FILE *outputfp;
+    outputfp = fopen("/home/nvidia/BarelangFC_ROS2/src/motion/src/source_code/Player/SensorNote", "r");
+    fscanf(outputfp, "%s", line);
+    spr = strtok(line, ";");
+    while (spr != NULL)
+    {
+        if (countParse == 0)
+        {
+            sscanf(spr, "%lf", &accelero_x);
+        } // x					#dpn-blkng
+        else if (countParse == 1)
+        {
+            sscanf(spr, "%lf", &accelero_y);
+        } // y					#kiri-kanan
+        else if (countParse == 2)
+        {
+            sscanf(spr, "%lf", &accelero_z);
+        } // z
+        else if (countParse == 3)
+        {
+            sscanf(spr, "%lf", &gyroscope_x);
+        } //				#kiri-kanan
+        else if (countParse == 4)
+        {
+            sscanf(spr, "%lf", &gyroscope_y);
+        } //				#dpn-blkng
+        else if (countParse == 5)
+        {
+            sscanf(spr, "%lf", &gyroscope_z);
+        } //
+        else if (countParse == 6)
+        {
+            sscanf(spr, "%lf", &roll);
+        } // roll
+        else if (countParse == 7)
+        {
+            sscanf(spr, "%lf", &pitch);
+        } // pitch
+        else if (countParse == 8)
+        {
+            sscanf(spr, "%lf", &yaw);
+        } // yaw
+        else if (countParse == 9)
+        {
+            sscanf(spr, "%d", &strategy);
+        } //
+        else if (countParse == 10)
+        {
+            sscanf(spr, "%d", &kill);
+            countParse = -1;
+        } //
+        spr = strtok(NULL, ";");
+        countParse++;
+    }   
+    fclose(outputfp);
 }
 
 int main(int argc, char **argv)
